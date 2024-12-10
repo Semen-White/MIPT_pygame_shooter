227efc7fb1f1e548ae40cb6885fbe34abb48e65b
Semen-White <belykh.sb@phystech.edu>
2024-12-11 02:00:39 +0300

    Add file

--- game.py
+++ game.py
@@ -15,19 +15,57 @@ walk_right = [pygame.image.load('images/player_right/player_right1.png'),
     pygame.image.load('images/player_right/player_right5.png'),
     pygame.image.load('images/player_right/player_right6.png'),
     pygame.image.load('images/player_right/player_right7.png'),
-    pygame.image.load('images/player_right/player_right8.png')
-]
-
+    pygame.image.load('images/player_right/player_right8.png')]
+walk_left = [pygame.image.load('images/player_left/player_left1.png'),
+    pygame.image.load('images/player_left/player_left2.png'),
+    pygame.image.load('images/player_left/player_left3.png'),
+    pygame.image.load('images/player_left/player_left4.png'),
+    pygame.image.load('images/player_left/player_left5.png'),
+    pygame.image.load('images/player_left/player_left6.png'),
+    pygame.image.load('images/player_left/player_left7.png'),
+    pygame.image.load('images/player_left/player_left8.png')]
 player_anim_count = 0
-
 backyard_x = 0
+
+player_speed = 10
+player_x = 100
+player_y = 320
+
+is_jump = False
+jump_count = 8
 
 running = True
 while running:
-
+    keys = pygame.key.get_pressed()
     screen.blit(backyard, (backyard_x,0))
     screen.blit(backyard, (backyard_x + 700,0))
-    screen.blit(walk_right[player_anim_count], (100, 320))
+
+    if keys[pygame.K_LEFT]:
+        screen.blit(walk_left[player_anim_count], (player_x, player_y))
+    else:
+        screen.blit(walk_right[player_anim_count], (player_x, player_y))
+
+
+
+
+    if keys[pygame.K_LEFT] and player_x > 50:
+        player_x -= player_speed
+    elif keys[pygame.K_RIGHT] and player_x < 600:
+        player_x += player_speed
+
+    if not is_jump:
+        if keys[pygame.K_UP]:
+            is_jump = True
+    else:
+        if jump_count >= -8:
+            if jump_count > 0:
+                player_y -= (jump_count ** 2) / 2
+            else:
+                player_y += (jump_count ** 2) / 2
+            jump_count -= 1
+        else:
+            is_jump = False
+            jump_count = 8
 
     if player_anim_count == 7:
         player_anim_count = 0
@@ -45,4 +83,4 @@ while running:
             running = False
             pygame.quit()
 
-    clock.tick(15)
+    clock.tick(20)
