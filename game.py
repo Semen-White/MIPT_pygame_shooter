c0ce24acb76d0bc9459b4f0adfb07a019ae989b0
unknown <animewizard314@gmail.com>
2024-12-17 12:53:03 +0300

    пробный коммит

--- game.py
+++ game.py
@@ -32,6 +32,8 @@ walk_left = [pygame.image.load('images/player_left/player_left1.png').convert_al
              pygame.image.load('images/player_left/player_left8.png').convert_alpha()]  # спрайт движения налево
 
 enemy = pygame.image.load('images/enemy/ghost.png').convert_alpha()
+enemy_death = pygame.image.load('images/enemy/fog.png').convert_alpha()
+enemy_death = pygame.transform.scale(enemy_death, (100, 100))
 enemy_list_in_game = []
 
 player_anim_count = 0
@@ -50,17 +52,16 @@ pygame.time.set_timer(enemy_timer, 2500)
 enemy_spawn_time = 0
 
 label = pygame.font.Font('fonts/Poppins/Poppins-BlackItalic.ttf', 35)
-lose_label = label.render('You lose :(', False, (232, 137, 218))
-restart_label = label.render('Restart?', False, (232, 137, 218))
+lose_label = label.render('You lose :(', True, (232, 137, 218))
+restart_label = label.render('Restart?', True, (232, 137, 218))
 restart_label_rect = restart_label.get_rect(topleft=(480, 75))
-play_label = label.render('Play', False, (232, 137, 218))
+play_label = label.render('Play', True, (232, 137, 218))
 play_label_rect = play_label.get_rect(topleft=(480, 75))
-start_label = label.render('Ghost Girl', False, (232, 137, 218))
+start_label = label.render('Ghost Girl', True, (232, 137, 218))
 
 fireballs_left = 10000
 fireball = pygame.image.load('images/fireball.png').convert_alpha()
-fireball_boom = pygame.image.load(
-    'images/explode (1).png').convert_alpha()  # <--------- вставить картинку взрыва файрболла
+fireball_boom = pygame.image.load('images/explode (1).png').convert_alpha()  # <--------- вставить картинку взрыва файрболла
 fireballs = []
 
 gameplay = True
@@ -72,34 +73,34 @@ while running:
     screen.blit(startscreen, (0, 0))
     screen.blit(play_label, play_label_rect)
     screen.blit(start_label, (400, 25))
-    mouse = pygame.mouse.get_pos()
-    if play_label_rect.collidepoint(mouse) and pygame.mouse.get_pressed()[0]:
+    mouse = pygame.mouse.get_pos()# <---- позиция мыши
+    if play_label_rect.collidepoint(mouse) and pygame.mouse.get_pressed()[0]: # <---- регистрация нажатия на кнопке
         start = True
 
     if start:
 
-        screen.blit(backyard, (backyard_x, 0))
+        screen.blit(backyard, (backyard_x, 0)) # <---- экран (х2)
         screen.blit(backyard, (backyard_x + 700, 0))
 
         if gameplay:  # основной игровой цикл
 
-            backyard_sound.play()
+            #backyard_sound.play() # <---- выключить при необходимости
             player_rect = walk_left[0].get_rect(topleft=(player_x, player_y))  # хитбокс игрока
 
-            if enemy_list_in_game:
+            if enemy_list_in_game: # <---- проверка наличия врага
                 for (i, el) in enumerate(enemy_list_in_game):
                     # enemy_spawn_time = rd.randint(1000,5000)
                     # pygame.time.set_timer(enemy_timer, enemy_spawn_time)
-                    screen.blit(enemy, el)
-                    # enemy_speed_mod = rd.randint(-10,10)
-                    el.x -= 5  # + enemy_speed_mod
+                    screen.blit(enemy, el)  # <---- появление врага
+                    enemy_speed_mod = rd.randint(-10,10) # <---- модификатор скорости (выключить при необходимости)
+                    el.x -= 5 + enemy_speed_mod
 
                     if el.x < -50:
                         enemy_list_in_game.pop(i)
 
-                    if player_rect.colliderect(el):
+                    if player_rect.colliderect(el): # <---- смерть игрока
                         gameplay = False
-            keys = pygame.key.get_pressed()
+            keys = pygame.key.get_pressed() # <---- модуль ходьбы и прыжков
 
             if keys[pygame.K_LEFT]:
                 screen.blit(walk_left[player_anim_count], (player_x, player_y))
@@ -130,10 +131,10 @@ while running:
                     player_anim_count = 0
                     player_anim_count_0 = 0
                 else:
-                    player_anim_count_0 += 0.1
+                    player_anim_count_0 += (ticks/100)/2
                     player_anim_count = int(player_anim_count_0)
 
-            backyard_x -= 2
+            backyard_x -= 2  # <---- движение заднего фона
             if backyard_x == -700:
                 backyard_x = 0
 
@@ -151,6 +152,7 @@ while running:
                             if el.colliderect(enemy_el):
                                 screen.blit(fireball_boom, (el.x, el.y))
                                 enemy_list_in_game.pop(index)
+                                screen.blit(enemy_death, (el.x + 30, el.y - 64))
                                 fireballs.pop(i)
 
         else:
